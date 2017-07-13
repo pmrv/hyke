ac0e7d06a881a4382364c020efab19ab416bbb57
Marvin Poul <ponder@creshal.de>
2017-07-13 18:03:45 +0200

    include package name in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(name = 'hyke',
-      version = '0.1.0',
+      version = '0.1.1',
       description = 'Utility to run scripts and capture their output in a '
                     'repeatable way',
       author = 'Marvin Poul',
       author_email = 'ponder@creshal.de',
       license = 'MIT',
+
+      packages = ["hyke"],
 
       entry_points = {
           'console_scripts': [
