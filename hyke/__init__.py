93bffc0eeb7874fae86d5c0d6e2d10a359f57778
Marvin Poul <ponder@creshal.de>
2017-07-18 16:10:53 +0200

    add mail & tag support, add some switches
    * send output to given email via local smtp server
    * add addtional by-tag/ path to run dir, to add arbitrary tags to runs
    * make the run dir configurable
    * add flag to just prepare run dir

--- hyke/__init__.py
+++ hyke/__init__.py
@@ -23,11 +23,23 @@ def main():
     parser.add_argument('SIM', help = 'name of the simulation script')
     parser.add_argument('-b', '--base', default = '~',
                         help = 'base directory')
+    parser.add_argument('-p', '--prepare-only', action = 'store_true',
+                        help = 'only prepare the run directory without '
+                               'executing the script, print the run diractory')
+    parser.add_argument('-r', '--run', default = 'runs',
+                        help = 'directory where runs should be stored.')
+    parser.add_argument('-t', '--tags', nargs = '*',
+                        help = 'tags describing the run')
+    parser.add_argument('-e', '--email', default = None,
+                        help = 'send script output to this mail on completion')
     parser.add_argument('-v', '--verbose', action = 'store_true',
                         help = 'show live script output')
     args = parser.parse_args(run_args)
 
-    hyke = Hyke(base = args.base, verbose = args.verbose)
-    hyke.create_directory()
-    hyke.prepare_directory(args.SIM)
-    hyke.execute_script(args.SIM, sim_args)
+    hyke = Hyke(base = args.base, run = args.run, verbose = args.verbose)
+    run_dir = hyke.create_directory()
+    hyke.prepare_directory(args.SIM, args.tags)
+    if args.prepare_only:
+        print(run_dir)
+    else:
+        hyke.execute_script(args.SIM, sim_args, email = args.email)
--- hyke/hyke.py
+++ hyke/hyke.py
@@ -1,16 +1,19 @@
+import email
 import json
 import os
 from os.path import expanduser
 import pathlib
 import shutil
+import smtplib
 import subprocess
+import sys
 import time
 
 TIME_FMT = '%Y-%m-%d_%H-%M-%S'
 
 class Hyke:
 
-    def __init__(self, base = '~', verbose = False):
+    def __init__(self, base = '~', run = 'runs', verbose = False):
         '''
         base    -- the base directory to use
         verbose -- if true, print the output of the script as it runs
@@ -20,10 +23,13 @@ class Hyke:
         self.verbose = verbose
         self.base = pathlib.Path(expanduser(base)).resolve()
         self.sim_base = self.base / 'sims'
-        self.run_base = self.base / 'runs'
+        self.run_base = self.base / run
 
         self.by_script_base = self.run_base / 'by-script'
         os.makedirs(str(self.by_script_base), exist_ok = True)
+
+        self.by_tag_base = self.run_base / 'by-tag'
+        os.makedirs(str(self.by_tag_base), exist_ok = True)
 
     def create_directory(self):
         '''
@@ -38,29 +44,25 @@ class Hyke:
         self.run_dir = run_dir
         return run_dir
 
-    def prepare_directory(self, script):
+    def prepare_directory(self, script, tags):
         '''
         Prepare the run directory so that the can be run inside. In particular copy
         all files from the sim_base directory into the directory and setup the
         by-* symlinks.
         '''
 
-        script = pathlib.Path(script)
+        script = pathlib.Path(self.sim_base / script)
         script_parent = script.parent
         script_name   = script.name
 
         for f in filter(lambda f: not f.is_dir(),
-                        (self.sim_base / script_parent).glob('*')):
+                        script_parent.glob('*')):
             shutil.copy(str(f), str(self.run_dir), follow_symlinks = True)
             target = self.run_dir / f.name
             # remove write permissions, to guard against accidental overwriting
             target.chmod(target.lstat().st_mode & (~0o222))
 
-        by_script_link = self.by_script_base / script
-        os.makedirs(str(by_script_link), exist_ok = True)
-        (by_script_link / self.run_dir.name).symlink_to(self.run_dir)
-
-        config_file = (self.sim_base / script).parent / '.hyke.json'
+        config_file = script_parent / '.hyke.json'
         if config_file.exists():
             with open(str(config_file)) as f:
                 conf = json.load(f)
@@ -82,9 +84,19 @@ class Hyke:
             except KeyError as e:
                 pass
 
-    def execute_script(self, script, sim_args):
+        by_script_link = self.by_script_base / script_name
+        os.makedirs(str(by_script_link), exist_ok = True)
+        (by_script_link / self.run_dir.name).symlink_to(self.run_dir)
+
+        for tag in tags:
+            by_tag_link = self.by_tag_base / tag
+            os.makedirs(str(by_tag_link), exist_ok = True)
+            (by_tag_link / self.run_dir.name).symlink_to(self.run_dir)
+
+    def execute_script(self, script, sim_args, send_email = None):
         '''
         Change into the run directory and execute the script.
+        If email is set, a mail with the script output to the given mail.
         '''
 
 
@@ -101,11 +113,29 @@ class Hyke:
             json.dump({'executable': 'python3', 'script': str(script),
                        'arguments': sim_args}, f)
 
+        output_path = str(self.run_dir / 'output')
         with subprocess.Popen(cli, cwd = str(self.run_dir),
                               universal_newlines = True,
                               stdout = subprocess.PIPE) as proc, \
-             open(str(self.run_dir / 'output'), 'w') as out:
+             open(output_path, 'w') as out:
                  for l in proc.stdout:
                      out.write(l)
                      if self.verbose:
                          print(l, end = '')
+
+        if not send_email: return
+
+        msg = email.message.Message()
+        msg["Subject"] = ("Failed! " if proc.returncode else "Success! ") \
+                       + ' '.join(cli)
+        msg["From"] = "simbot"
+        msg["To"] = send_email
+
+        with open(output_path, 'rb') as of:
+            msg.set_payload(of.read())
+
+        try:
+            with smtplib.SMTP("localhost") as smtp:
+                smtp.send_message(msg)
+        except (smtplib.SMTPException, ConnectionRefusedError):
+            print("Failed to send notification email!", file = sys.stderr)
